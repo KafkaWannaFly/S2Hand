8364bc871dc470cbdee71e549dec9d799e355f2e
Kafka Wanna Fly <47100782+tarzanchemgio@users.noreply.github.com>
2021-06-10 15:16:31 +0700

    Redux and Font Awesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App(): JSX.Element {
 	return (
