3a652507380564211fa16c9fb3a9b403b0a4157d
Kafka Wanna Fly <47100782+tarzanchemgio@users.noreply.github.com>
2021-06-10 21:27:31 +0700

    Debug set-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App(): JSX.Element {
 	return (
