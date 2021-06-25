e59b9e8277f8f098099b9f4ed1684423bea7dd44
Kafka Wanna Fly <47100782+tarzanchemgio@users.noreply.github.com>
2021-06-25 22:53:34 +0700

    Squashed commit of the following:
    commit 387e677e17f200ca279f963ff5d558ad81f654af
    Author: Kafka Wanna Fly <47100782+tarzanchemgio@users.noreply.github.com>
    Date:   Fri Jun 25 22:08:52 2021 +0700
    
        Navbar (#2)

--- src/res/index.ts
+++ src/res/index.ts
@@ -1,4 +1,4 @@
-import { FontFamily } from "./FontFamily";
+// import { FontFamily } from "./FontFamily";
 import { NavListPages, NavListActions, NavItemType } from "./NavigationConfig";
 import { CategoryList, CategoryItemType } from "./CategoryConfig";
 
