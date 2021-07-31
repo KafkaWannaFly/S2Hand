4b499c4f7cdf4ffbf4324a460e869d6c0037d061
quynhnhu-512000 <dangnguyenquynhnhu512000@gmail.com>
2021-07-31 23:51:25 +0700

    nhudang

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,11 +1,68 @@
 import React from "react";
+import styles from "./Login.module.scss";
 
-const Login = () => {
-  return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  );
-};
+// const Login = () => {
+//   return (
+//     <div>
+//       <h1>Login</h1>
+//     </div>
+//   );
+// };
+// export default Login;
 
+import { Link } from 'react-router-dom'
+
+class Login extends React.Component {
+	render() {
+		return (
+			<div>
+				{/* Page Content */}
+				<div className={styles["login"]}>
+					<div className={styles["login__container"]}>
+						<div className={styles["login__input-wrapper"]}>
+							<div className={styles["login__input"]}>
+								<input type="text" placeholder="Số điện thoại hoặc email">
+
+								</input>
+							</div>
+						</div>
+						<div className={styles["login__input-wrapper"]}>
+							<div className={styles["login__input"]}>
+								<input type="text" placeholder="Mật khẩu">
+
+								</input>
+							</div>
+						</div>
+						<div className={styles["login__input-wrapper"]}>
+							<div className={styles["login__keep-login"]}>
+								<input type="checkbox">
+
+								</input>
+								<label>
+									Duy trì đăng nhập
+								</label>
+							</div>
+
+							<div>
+								Quên mật khẩu
+							</div>
+						</div>
+						<div>
+							<button>
+								Đăng nhập
+							</button>
+						</div>
+						<div>
+							<button>
+								Tạo tài khoản mới
+							</button>
+						</div>
+
+					</div>
+				</div>
+				{/* Page Content */}
+			</div>
+		)
+	}
+}
 export default Login;
